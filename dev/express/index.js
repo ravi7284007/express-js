988947fb07285a5c86fa60a4d540b5de97b800b0
Ravi Kumar <ravi7284007@gmail.com>
2025-07-03 17:03:02 +0530

    changes for .env file

--- dev/express/index.js
+++ dev/express/index.js
@@ -1,16 +1,30 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+/*
+app.get("/", (req, res) => {
+  res.send("Hello from Hitesh and his tea!");
+});
+
+app.get("/ice-tea", (req, res) => {
+  res.send("What ice tea would you prefer?");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("hiteshdotcom");
+});
+*/
 
 app.use(express.json());
 
 let teaData = [];
 let nextId = 1;
 
-// add a new hardware
-app.post("/hardware", (req, res) => {
+// add a new tea
+app.post("/teas", (req, res) => {
   const { name, price } = req.body;
   const newTea = {
     id: nextId++,
@@ -21,43 +35,43 @@ app.post("/hardware", (req, res) => {
   res.status(201).send(newTea);
 });
 
-// get all hardware
-app.get("/hardware", (req, res) => {
+// get all teas
+app.get("/teas", (req, res) => {
   res.status(200).send(teaData);
 });
 
-// get a hardware with id
-app.get("/hardware/:id", (req, res) => {
+// get a tea with id
+app.get("/teas/:id", (req, res) => {
   const tea = teaData.find((tea) => tea.id === parseInt(req.params.id));
   if (!tea) {
-    return res.status(404).send("Hardware not found");
+    return res.status(404).send("Tea not found");
   }
 
   res.status(200).send(tea);
 });
 
-// update hardware
-app.put("/hardware/:id", (req, res) => {
-  const hardware = hardwareData.find((hardware) => hardware.id === parseInt(req.params.id));
-  if (!hardware) {
-    return res.status(404).send("Hardware not found");
+// update tea
+app.put("/teas/:id", (req, res) => {
+  const tea = teaData.find((tea) => tea.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Tea not found");
   }
 
   const { name, price } = req.body;
-  hardware.name = name;
-  hardware.price = price;
+  tea.name = name;
+  tea.price = price;
 
-  res.status(200).send(hardware);
+  res.status(200).send(tea);
 });
 
-// delete hardware
-app.delete("/hardware/:id", (req, res) => {
-  const index = hardwareData.findIndex((hardware) => hardware.id === parseInt(req.params.id));
+// delete tea
+app.delete("/teas/:id", (req, res) => {
+  const index = teaData.findIndex((tea) => tea.id === parseInt(req.params.id));
   if (index === -1) {
-    return res.status(404).send("Hardware not found");
+    return res.status(404).send("Tea not found");
   }
 
-  hardwareData.splice(index, 1);
+  teaData.splice(index, 1);
   res.status(204).send("Deleted");
 });
 
